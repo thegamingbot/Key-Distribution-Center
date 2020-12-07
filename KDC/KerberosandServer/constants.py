5a842cfee957e4f2a3c184fa8998f594318ad26c
thegamingbot <saikaushik609@gmail.com>
2020-12-07 11:11:26 +0530

    Servers Added

--- KDC/KerberosandServer/constants.py
+++ KDC/KerberosandServer/constants.py
@@ -1,5 +1,5 @@
 IPsAndPorts = {
-    "Chat App Server": ['34.70.4.193', 10001],
+    "Chat App Server": ['34.70.4.193', 10002],
     "Quiz Server": ['127.0.0.3', 11001],
     "File Transfer Server": ['127.0.0.4', 12001]
 }
