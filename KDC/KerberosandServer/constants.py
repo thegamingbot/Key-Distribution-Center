baf669e11a7d54a57579914b6fc969f6d5aa624a
thegamingbot <saikaushik609@gmail.com>
2020-12-07 13:12:28 +0530

    Servers Added

--- KDC/KerberosandServer/constants.py
+++ KDC/KerberosandServer/constants.py
@@ -1,7 +1,7 @@
 IPsAndPorts = {
-    "Chat App Server": ['34.67.5.235', 10001],
-    "Quiz Server": ['127.0.0.3', 11001],
-    "File Transfer Server": ['127.0.0.4', 12001]
+    "Chat App Server": ['34.67.5.235', 9010],
+    "Quiz Server": ['127.0.0.3', 9011],
+    "File Transfer Server": ['127.0.0.4', 9012]
 }
 ASPort = 9009
 TGSPort = 9008
